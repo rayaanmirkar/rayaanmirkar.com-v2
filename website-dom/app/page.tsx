8d5c955b66fc18582a6b16d5289b09e18fe497a4
Rayaan Mirkar <ilmrexplorer@gmail.com>
2026-08-01 02:37:32 -0400

    Update page.tsx

--- website-dom/app/page.tsx
+++ website-dom/app/page.tsx
@@ -115,7 +115,7 @@ export default function Home() {
             </p>
             <div className="mt-6 space-y-5">
               <p className="body-copy">
-                I mainly work on my own, indepedent, computational pipelines and other research simulations, though I am open to collaborating on potential projects.
+                I mainly work on my own, indepedent, research projects, though I am open to collaborating on potential projects.
               </p>
               <p className="body-copy">
                 My current work centers on bacteriophage proteomes, long-range neural sequence models, and efficient
